d0cc8dd9726dd9f8f1b646fba1f96d4ba2a1e0f3
tonyroy <nomail@gmail.com>
2013-05-13 18:23:29 +1000

    Initial commit

---
+++ js/losscalc.js
@@ -0,0 +1,79 @@
+
+function safeNum(value) {
+    if ( isNaN(value) )
+            return 0;
+    else
+            return parseFloat(value);
+};
+
+
+
+function CalculatorViewModel() {
+    var self = this;
+    self.distanceUnits =  [
+            {name:'Kilometres',value: 1},
+            {name:'Metres', value: .001},
+            {name:'Miles', value: 1.6093},
+            {name:'Feet', value: .0016093},
+            
+    ];
+    self.cableLength = ko.observable(20);
+    self.selectedDistanceUnits = ko.observable(1);
+    self.adjustedDistance = ko.computed(function() {
+            return self.cableLength() * self.selectedDistanceUnits().value
+    });
+    
+    self.numberOfConnectors = ko.observable(2);
+    self.numberOfSplices = ko.observable(1);
+    
+    self.availableStandards = standards;
+    self.selectedStandard = ko.observable(standards[0]);
+    
+
+    self.splitterLoss = ko.observable();
+    self.switchLoss = ko.observable();
+    self.otherLoss = ko.observable();
+    
+    self.connectorLoss = ko.computed(function() {
+        if ( self.selectedStandard) {
+            return safeNum(self.selectedStandard().connectorLoss * self.numberOfConnectors() );
+        }			
+    });
+    
+    self.totalOtherLoss = ko.computed(function() {
+        val = safeNum(self.splitterLoss()) + safeNum(self.switchLoss()) + safeNum(self.otherLoss());
+        return val;
+    });
+    
+    self.spliceLoss = ko.computed(function() {
+        if ( self.selectedStandard ) {
+            return safeNum(self.selectedStandard().spliceLoss * self.numberOfSplices() );
+        }
+    });
+
+
+    
+    self.validParamters = ko.computed(function() {
+            return true;
+    });
+    
+    self.lossResults = ko.computed( function() {
+        var lossValues = [];
+        ko.utils.arrayForEach(self.selectedStandard().testWavelengths,function(wl) {
+            var fibreLoss =  self.adjustedDistance() * wl.lossPerKm    
+            var totalLoss = fibreLoss + self.spliceLoss() + self.connectorLoss() + self.totalOtherLoss(); 
+            lossValues.push(
+                { testWavelength: wl ,
+                  fibreLoss: fibreLoss, 
+                  totalLoss: totalLoss 
+                });
+        });
+        return lossValues;
+    });
+    
+
+    
+
+};
+
+
